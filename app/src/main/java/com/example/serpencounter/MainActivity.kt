1d407f53ebfffe3d7dfb3df4769a10036037dfda
JurajNznsk <juraj.niznansky@gmail.com>
2025-04-19 20:53:40 +0200

    StartingScreen.kt: added | trying to create Button

--- app/src/main/java/com/example/serpencounter/MainActivity.kt
+++ app/src/main/java/com/example/serpencounter/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.serpencounter.ui.theme.Screens.StartingScreen
 import com.example.serpencounter.ui.theme.SerpEncounterTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             SerpEncounterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    StartingScreen(
+                        onClick = {},
+                        modifier = Modifier
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SerpEncounterTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/serpencounter/ui/theme/Screens/StartingScreen.kt
@@ -0,0 +1,21 @@
+package com.example.serpencounter.ui.theme.Screens
+
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StartingScreen (
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val text = "Encounter"
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier.widthIn(min = 250.dp)
+    ) {
+    }
+}
