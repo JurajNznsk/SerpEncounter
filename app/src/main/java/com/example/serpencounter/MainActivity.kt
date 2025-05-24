9b09474f08222110d20e019b1fc5f38dc2c168ce
JurajNznsk <juraj.niznansky@gmail.com>
2025-05-24 20:53:55 +0200

    Restart

--- app/src/main/java/com/example/serpencounter/MainActivity.kt
+++ app/src/main/java/com/example/serpencounter/MainActivity.kt
@@ -3,13 +3,11 @@ package com.example.serpencounter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.serpencounter.ui.Navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
         }
     }
 }
