654de63b0d74566f399e7694fdb43f50d25eac16
JurajNznsk <juraj.niznansky@gmail.com>
2025-05-24 19:51:56 +0200

    StartScreen.kt: renamed

--- app/src/main/java/com/example/serpencounter/ui/theme/Screens/StartScreen.kt
+++ app/src/main/java/com/example/serpencounter/ui/theme/Screens/StartScreen.kt
@@ -2,13 +2,12 @@ package com.example.serpencounter.ui.theme.Screens
 
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun StartingScreen (
+fun StartScreen (
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
