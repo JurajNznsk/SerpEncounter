401459f18fdfb9211cbdbed286168c0019a10e66
JurajNznsk <juraj.niznansky@gmail.com>
2025-06-02 14:24:51 +0200

    StartSceen: reworked alignment

--- app/src/main/java/com/example/serpencounter/ui/screens/StartScreen.kt
+++ app/src/main/java/com/example/serpencounter/ui/screens/StartScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -15,24 +16,38 @@ import com.example.serpencounter.R
 
 @Composable
 fun StartScreen() {
-    Box() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Background stone bricks
         Image(
             painter = painterResource(id = R.drawable.background),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-        Column {
+        Column (
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Logo
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null
+            )
+            // Encounter button
             OutlinedButton(
                 onClick = {}
             ) {
                 Text(text = stringResource(id = R.string.encounter_button))
             }
+            // List of Entities button
             OutlinedButton(
                 onClick = {}
             ) {
                 Text(text = stringResource(id = R.string.entity_list_button))
             }
+            // Saved Enc button
             OutlinedButton(
                 onClick = {}
             ) {
