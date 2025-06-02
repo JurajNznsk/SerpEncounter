09bd5844a739bc9c2d80cbd7a0448540e03bb36e
JurajNznsk <juraj.niznansky@gmail.com>
2025-06-02 13:07:40 +0200

    StartScreen: button1

--- app/src/main/java/com/example/serpencounter/MainActivity.kt
+++ app/src/main/java/com/example/serpencounter/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.serpencounter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.serpencounter.ui.screens.StartScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            StartScreen()
         }
     }
 }
---
+++ app/src/main/java/com/example/serpencounter/ui/screens/StartScreen.kt
@@ -0,0 +1,17 @@
+package com.example.serpencounter.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StartScreen() {
+    Column {
+        OutlinedButton(
+            onClick = {}
+        ) {
+            Text("Start")
+        }
+    }
+}
