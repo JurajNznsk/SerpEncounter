804902910367518ca946d9e90f0482f6315a3147
JurajNznsk <juraj.niznansky@gmail.com>
2025-06-02 17:28:16 +0200

    Enum - SerpEncScreens: added

--- app/src/main/java/com/example/serpencounter/SerpEnc.kt
+++ app/src/main/java/com/example/serpencounter/SerpEnc.kt
@@ -1,10 +1,19 @@
 package com.example.serpencounter
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+enum class SerpEncScreens(@StringRes val title: Int) {
+    Start(title = R.string.start_screen),
+    Encounter(title = R.string.enc_screen),
+    List(title = R.string.list_screen),
+    Saved(title = R.string.saved_screen);
+}
 
 @Composable
 fun SerpEncApp(
 
 ) {
-
+    val navController = rememberNavController()
 }
