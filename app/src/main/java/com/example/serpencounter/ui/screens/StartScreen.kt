f4c6ca32082a8733b34ae1e8a59ee0f1ac664ac0
JurajNznsk <juraj.niznansky@gmail.com>
2025-06-02 13:59:34 +0200

    StartScreen: background image; Box layout; 3 buttons

--- app/src/main/java/com/example/serpencounter/ui/screens/StartScreen.kt
+++ app/src/main/java/com/example/serpencounter/ui/screens/StartScreen.kt
@@ -1,29 +1,43 @@
 package com.example.serpencounter.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.example.serpencounter.R
 
 @Composable
 fun StartScreen() {
-    Column {
-        OutlinedButton(
-            onClick = {}
-        ) {
-            Text(text = stringResource(id = R.string.encounter_button))
-        }
-        OutlinedButton(
-            onClick = {}
-        ) {
-            Text(text = stringResource(id = R.string.entity_list_button))
-        }
-        OutlinedButton(
-            onClick = {}
-        ) {
-            Text(text = stringResource(id = R.string.saved_encounters_button))
+    Box() {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column {
+            OutlinedButton(
+                onClick = {}
+            ) {
+                Text(text = stringResource(id = R.string.encounter_button))
+            }
+            OutlinedButton(
+                onClick = {}
+            ) {
+                Text(text = stringResource(id = R.string.entity_list_button))
+            }
+            OutlinedButton(
+                onClick = {}
+            ) {
+                Text(text = stringResource(id = R.string.saved_encounters_button))
+            }
         }
     }
 }
