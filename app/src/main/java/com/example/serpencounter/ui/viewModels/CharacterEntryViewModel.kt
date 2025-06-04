45447f72d88b7ad5ced0902a7a5667c0725eaba9
JurajNznsk <juraj.niznansky@gmail.com>
2025-06-04 20:48:02 +0200

    TODO added

--- app/src/main/java/com/example/serpencounter/ui/viewModels/CharacterEntryViewModel.kt
+++ app/src/main/java/com/example/serpencounter/ui/viewModels/CharacterEntryViewModel.kt
@@ -4,5 +4,5 @@ import androidx.lifecycle.ViewModel
 import com.example.serpencounter.data.CharacterRepository
 
 class CharacterEntryViewModel(private val characterRepository: CharacterRepository) : ViewModel() {
-
+    //TODO: entry of new char to DB
 }
