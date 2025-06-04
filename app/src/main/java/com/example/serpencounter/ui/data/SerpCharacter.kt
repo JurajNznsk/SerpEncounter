0ee80df8261e4b4664397308180ef0223f66be62
JurajNznsk <juraj.niznansky@gmail.com>
2025-06-04 16:50:18 +0200

    SerpCharacter: added (data class - table)

---
+++ app/src/main/java/com/example/serpencounter/ui/data/SerpCharacter.kt
@@ -0,0 +1,13 @@
+package com.example.serpencounter.ui.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "characters")
+data class SerpCharacter(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val maxHP: Int,
+    val armorClass: Int,
+    val imageRes: Int
+)
