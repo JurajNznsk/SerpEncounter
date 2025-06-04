1d352ddaf47b8e52ad15b96bff61e5d930725cb3
JurajNznsk <juraj.niznansky@gmail.com>
2025-06-04 17:03:49 +0200

    CharacterDatabase: singleton

--- app/src/main/java/com/example/serpencounter/ui/data/CharacterDatabase.kt
+++ app/src/main/java/com/example/serpencounter/ui/data/CharacterDatabase.kt
@@ -1,9 +1,25 @@
 package com.example.serpencounter.ui.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [SerpCharacter::class], version = 1, exportSchema = false)
 abstract class CharacterDatabase : RoomDatabase() {
     abstract fun serpCharacterDao(): SerpCharacterDao
+
+    companion object {
+        @Volatile
+        private var Instance: CharacterDatabase? = null
+
+        fun getDatabase(context: Context): CharacterDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, CharacterDatabase::class.java, "character-database")
+                    .fallbackToDestructiveMigration(false) // Destroys data from tables when no migration path defined (when attempt to migrate)
+                    .build()
+                    .also { Instance = it } // Stores DB into Instance
+            }
+        }
+    }
 }
