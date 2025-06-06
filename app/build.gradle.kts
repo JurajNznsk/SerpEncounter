333b2debeb3db92d73246462ed47b93ec839f3d1
JurajNznsk <juraj.niznansky@gmail.com>
2025-06-06 15:42:21 +0200

    Removed gson dependancy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,5 +63,4 @@ dependencies {
     val room_version = "2.7.1"
     implementation("androidx.room:room-runtime:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
 }
